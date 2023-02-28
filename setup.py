2c598be8fc22ae5e83b427ea13a5bb564456e9c0
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-03-01 01:21:21 +0530

    remove __version

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from main_1 import __version__
 
 setup(
     name='downloader',
-    version=__version__,
 
     url='https://github.com/jayachandra27/pdf_cli',
     author='Jay Reddy',
     author_email="jayachandra.s.reddy@gmail.com",
     
-    py_modules=["main1"],
+    py_modules=["main_1"],
 )
